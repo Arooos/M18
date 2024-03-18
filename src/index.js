1112b99cae7f994afdece29614b96ded2960973b
Artem <artemosipov111@gmail.com>
2024-03-18 16:43:42 +0300

    Адаптация завершена. Проект завершен

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 //слайдер
+
 const item = document.querySelectorAll('.slider .slider-wrap .slider-line .item');
 const sliderLine = document.querySelector('.slider .slider-wrap .slider-line');
 let count = 0;
 let width;
 
 function init() {
-    console.log('resize');
     width = document.querySelector('.slider .slider-wrap').offsetWidth;
     sliderLine.style.width = width * item.length + 'px';
     item.forEach( item => {
         item.style.width = width + 'px';
         item.style.height = 'auto';
     });
-    rollSlider()
-    console.log(width);
+    rollSlider();
 }
 
 window.addEventListener('resize', init);
@@ -24,14 +23,14 @@ document.querySelector('.slider-next').addEventListener('click', function(){
     if (count >= item.length) {
         count = 0;
     };
-    rollSlider()
+    rollSlider();
 });
 
 document.querySelector('.slider-prev').addEventListener('click', function () {
     count--;
     if (count < 0) {
         count = item.length - 1;
-    }
+    };
     rollSlider();
 });
 
@@ -53,28 +52,28 @@ btn.onclick = function () {
     document.body.classList.add('bg-block');
     modal.classList.add('active');
     modal_sent.classList.add('active');
-}
+};
 
 modal_close.onclick = function () {
     document.body.classList.remove('bg-block');
     modal.classList.remove('active');
     modal_sent.classList.remove('active');
-}
+};
 
 window.onclick = function (event) {
     if (event.target == modal_bg) {
         document.body.classList.remove('bg-block');
         modal.classList.remove('active');
         modal_sent.classList.remove('active');
-    }
-}
-
-
-
+    };
+};
 
 const modalBtns = document.querySelectorAll('.modal-open');
 const modals = document.querySelectorAll('.modal');
 
 function openModal(elem) {
-    elem.classList.add('active')
-}
+    elem.classList.add('active');
+};
+
+
+
